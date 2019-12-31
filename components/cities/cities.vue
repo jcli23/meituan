42026c5e7a30ced15a16cded893fd57188b63373
jcli23 <623896069@qq.com>
2019-12-31 16:30:37 +0800

    更新提交

--- components/cities/cities.vue
+++ components/cities/cities.vue
@@ -31,16 +31,19 @@
                 </div>
               </div>
             </div>
-            <img :src="lock2===true?'../../assets/images/top_jiao.png':''" alt class="jiao" />
+            <img v-if="lock2===true" src="../../assets/images/top_jiao.png" alt class="jiao" />
             <div v-if="lock2===true" class="line"></div>
           </div>
-        
-        
         </div>
         <div class="search">
           <div class="font">直接搜索：</div>
           <div class="input">
-            <input v-model="value" placeholder="请输入城市中文或拼音" />
+            <input v-model="value" placeholder="请输入城市中文或拼音" @input="searchvalue(value)" @blur="searchvalue(value)" />
+            <!-- <div class="resultbox" v-if="searchlist.length>0">
+              <div v-for="(item,index) in searchlist" :key="index">
+                <div>{{item}}</div>
+              </div>
+            </div> -->
           </div>
         </div>
       </div>
@@ -53,29 +56,62 @@
       </div>
       <div class="pysearch">
         <div class="font">按拼音首字母查找：</div>
-        <!-- <div v-for="(item,index) in citylist.data.cities" :key="index" class="for">
-          <div>{{}}</div>
-        </div> -->
+        <div v-for="(item,index) in letter" :key="index" class="for">
+          <a :href="'#'+item" class="letter">{{item}}</a>
+        </div>
+      </div>
+      <div class="citylist" v-for="(item,index) in letter" :key="index">
+        <div :id="item" class="letter">{{item}}</div>
+        <div class="right">
+          <div v-for="(item1,index1) in citylist[item]" :key="index1" class="for">
+            <div class="name" @click="city(item1.name)">{{item1.name}}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import citylist from "../../assets/city";
+import citylist from "../../assets/city";
 export default {
   data() {
     return {
       province: [],
       chooseprovince: null,
       choosecity: null,
-      value:'',
+      value: "",
       lock1: false,
       lock2: false,
       id: 0,
-      cities:[],
-      hotcity:[],
-      citylist:{}
+      searchresult:[],
+      cities: [],
+      hotcity: [],
+      citylist: {},
+      letter: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "W",
+        "X",
+        "Y",
+        "Z"
+      ]
     };
   },
   components: {},
@@ -104,45 +140,56 @@ export default {
     },
     getcities() {
       this.$axios
-          .get(`citys/${this.id}`)
-          .then(res => {
-              this.cities=res.data.data.city
-              console.log(this.cities)
-          })
-          .catch(err => {
-            console.log(err);
-          });
+        .get(`citys/${this.id}`)
+        .then(res => {
+          this.cities = res.data.data.city;
+          console.log(this.cities);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
     open1() {
       this.lock1 = !this.lock1;
       this.lock2 = false;
     },
     open2() {
-      if(this.chooseprovince!==null){
+      if (this.chooseprovince !== null) {
         this.lock1 = false;
         this.lock2 = !this.lock2;
       }
     },
     choosepro(e, id) {
-      (this.chooseprovince = e),
-        (this.id = id),
-      this.getcities()
+      (this.chooseprovince = e), (this.id = id), this.getcities();
       this.lock1 = false;
     },
-    
+
     choosepro2(e) {
-      (this.choosecity = e),
-      localStorage.setItem('city',this.choosecity)
-      localStorage.setItem('chooseprovince',this.chooseprovince)
+      (this.choosecity = e), localStorage.setItem("city", this.choosecity);
+      localStorage.setItem("chooseprovince", this.chooseprovince);
       this.$router.push("/");
       this.lock2 = false;
     },
-    
+    city(e){
+      localStorage.setItem("city",e);
+      this.$router.push("/");
+    },
+    searchvalue(value){
+       if(value.length>0){
+         this.letter.map(item=>{
+           this.citylist[item].map(item1=>{
+             if(item1.spell.indexOf(value)>-1 ||item1.name.indexOf(value)>-1){
+                this.searchresult.push(item1.name)
+             }
+           })
+         })
+       }
+    }
   },
   mounted() {
     this.getprovince();
     this.gethotcity();
-    // this.citylist=citylist
+    this.citylist = citylist.data.cities;
   },
   watch: {},
   computed: {}
@@ -370,32 +417,32 @@ export default {
         }
       }
     }
-    .search{
+    .search {
       display: flex;
       width: 310px;
       height: 40px;
       margin: 0 0 0 40px;
       align-items: center;
-      .font{
+      .font {
         font-size: 16px;
         color: #333;
       }
-      .input{
+      .input {
         width: 230px;
         height: 40px;
         border: 1px solid #cdcdcd;
         padding: 3px 0 3px 5px;
         border-radius: 4px;
-        input{
-           width: 100%;
-           height: 100%;
-           border: none;
-           outline: none;
+        input {
+          width: 100%;
+          height: 100%;
+          border: none;
+          outline: none;
         }
       }
     }
   }
-  .hotcity{
+  .hotcity {
     width: 100%;
     height: 80px;
     padding: 30px 0;
@@ -403,31 +450,75 @@ export default {
     border-bottom: 1px solid #cdcdcd;
     display: flex;
     align-items: center;
-    .font{
+    .font {
       font-size: 16px;
       color: #333;
     }
-    .for{
+    .for {
       margin: 0 20px;
-      .title{
+      .title {
         color: #666;
         font-size: 14px;
       }
     }
   }
-  .pysearch{
+  .pysearch {
     width: 100%;
     height: 80px;
     padding: 30px 0;
     border-top: 1px solid #cdcdcd;
-    border-bottom: 1px solid #cdcdcd;
     display: flex;
     align-items: center;
-    .font{
+    .font {
       font-size: 16px;
       color: #333;
     }
-    
+    .for {
+      margin: 0 10px;
+      width: 25px;
+      height: 25px;
+      text-align: center;
+      line-height: 25px;
+      .letter {
+        font-size: 15px;
+        color: #222;
+        &:hover {
+          color: #feb655;
+        }
+      }
+    }
+  }
+  .citylist {
+    width: 100%;
+    padding: 13px 30px 13px 10px;
+    display: flex;
+    &:hover{
+      background: #f8f8f8;
+    }
+    .letter{
+      width: 40px;
+      height: 40px;
+      background: #ffd000;
+      border-radius: 50%;
+      text-align: center;
+      line-height: 40px;
+      font-size: 16px;
+      color: #222;
+    }
+    .right{
+      width: 1065px;
+      display: flex;
+      flex-wrap: wrap;
+      .for{
+        margin: 10px 20px;
+        .name{
+          font-size: 14px;
+          &:hover {
+          color: #feb655;
+        }
+        }
+      }
+    }
   }
 }
 </style>
